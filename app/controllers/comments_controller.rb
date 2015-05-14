3d8344e46597e2edd9d099149da9968cf3deb754
Muhammad Aamir Talib <aaamir.talib@gmail.com>
2015-05-14 15:22:33 +0500

    fixes and improments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,64 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:update, :destroy]
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @post, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
